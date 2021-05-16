27c5c883b867a44cc1cb2dc6b92eee2b1be61580
Alen Yohannan <erwinsmithscout1@gmail.com>
2021-05-16 16:18:37 +0530

    Better Tests

--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,5 @@
-const Spotify = require('../dist/Spotify')
-const spotify = new Spotify.SpotifyFetcher({
+const Spotify = require('../dist/Spotify').default
+const spotify = new Spotify({
     clientId: 'acc6302297e040aeb6e4ac1fbdfd62c3',
     clientSecret: '0e8439a1280a43aba9a5bc0a16f3f009',
 })
@@ -11,4 +11,4 @@ const links = {
     song: 'https://open.spotify.com/track/1Ub6VfiTXgyV8HnsfzrZzC?si=4412ef4ebd8141ab'
 }
 
-spotify.verifyCredentials().then(() => Promise.all([spotify.getTrack(links.song), spotify.getAlbum(links.album), spotify.getArtist(links.artist)]).then(console.log))
+spotify.verifyCredentials().then(() => Promise.all([spotify.getTrack(links.song), spotify.getAlbum(links.album), spotify.getArtistAlbums(links.artist)]).then(console.log))
